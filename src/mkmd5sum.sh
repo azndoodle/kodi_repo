6fff5f8383ebe2319b0e80c744d61d52b0c71b77
Franklin <franklingo@gmail.com>
2022-01-02 22:24:47 -0500

    update readme, new checksum and enhance md5 script

--- src/mkmd5sum.sh
+++ src/mkmd5sum.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-md5sum addons.xml | awk '{printf("%s", $1)}' > addons.xml.md5
+md5sum ../zips/addons.xml | awk '{printf("%s", $1)}' > ../zips/addons.xml.md5
+
+## on mac comment out above and uncomment below
+#sudo md5 -q ../zips/addons.xml >  ../zips/addons.xml.md5
