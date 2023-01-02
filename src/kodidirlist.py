50bcc28fda62840f396f00ddcd0a54e4980a5e9a
Franklin <franklingo@gmail.com>
2023-01-02 18:06:18 -0500

    update script and repo index.html

--- src/kodidirlist.py
+++ src/kodidirlist.py
@@ -7,13 +7,13 @@ def print_sizes(root):
         if os.listdir(root) != None and name.find('.zip') == -1:
             continue
         #print '<tr><td><a href="%s">%s</a></td><td>%dB</td></tr>' % (name, name, os.stat(name).st_size)
-        print('<tr><td><a href="' + name + '">' + name + '</a></td><td>' + str(os.stat(name).st_size) + 'B</td></tr>')
+        print('      <tr><td><a href="' + name + '">' + name + '</a></td><td>' + str(os.stat(name).st_size) + 'B</td></tr>')
 
 
 print ('<html>')
-print ('<body>')
-print ('<table>')
+print ('  <body>')
+print ('    <table>')
 print_sizes('.')
-print ('</table>')
-print ('</body>')
+print ('    </table>')
+print ('  </body>')
 print ('</html>')
